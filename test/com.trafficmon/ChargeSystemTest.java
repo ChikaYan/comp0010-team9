213cdcf9b7ab52485a7a68ff5d9f3f561ad45a03
Haonan Zhang <32365701+HaonanZhang99@users.noreply.github.com>
2018-11-19 12:55:46 +0000

    Updated vehicle entering test

--- test/com.trafficmon/ChargeSystemTest.java
+++ test/com.trafficmon/ChargeSystemTest.java
@@ -6,7 +6,15 @@ import static org.junit.Assert.*;
 public class ChargeSystemTest {
     @Test
     public void checkVehicleEnteringZone(){
-        //test
+        CongestionChargeSystem ccs = new CongestionChargeSystem();
+        Vehicle vehicle = Vehicle.withRegistration("A123 XYZ");
+        ccs.vehicleEnteringZone(Vehicle vehicle);
+
+    }
+
+    @Test
+    public void checkVehicleLeavingZone(){
+
     }
 //    public void checkMinutesBetween(){
 //        CongestionChargeSystem ccs = new CongestionChargeSystem();
