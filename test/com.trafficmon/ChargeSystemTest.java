6e1c338d1010da54c1d18307b302a65b1dae60f0
Walter Wu <walterwuyan@gmail.com>
2018-11-19 12:54:02 +0000

    Added jmock lib

--- test/com.trafficmon/ChargeSystemTest.java
+++ test/com.trafficmon/ChargeSystemTest.java
@@ -1,9 +1,20 @@
 package com.trafficmon;
 
+import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
 
 public class ChargeSystemTest {
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
+
+    @Test
+    public void enterBefore2AndStayUpTo4IsChargedFor6(){
+
+    }
+
     @Test
     public void checkVehicleEnteringZone(){
         //test
