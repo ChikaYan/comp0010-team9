046917137cbf2fa2d99c16527171c8b8e0d012a0
Haonan Zhang <32365701+HaonanZhang99@users.noreply.github.com>
2018-11-21 16:14:42 +0000

    Update ChargeSystemTest.java

--- test/com.trafficmon/ChargeSystemTest.java
+++ test/com.trafficmon/ChargeSystemTest.java
@@ -12,10 +12,10 @@ public class ChargeSystemTest {
 
     @Test
     public void enterBefore2AndStayUpTo4IsChargedFor6(){
-
+        
     }
 
-<<<<<<< HEAD
+
 //    @Test
 //    public void checkVehicleEnteringZone(){
 //        CongestionChargeSystem ccs = new CongestionChargeSystem();
@@ -28,21 +28,6 @@ public class ChargeSystemTest {
 //    public void checkVehicleLeavingZone(){
 //
 //    }
-=======
-    @Test
-    public void checkVehicleEnteringZone(){
-        CongestionChargeSystem ccs = new CongestionChargeSystem();
-        Vehicle vehicle = Vehicle.withRegistration("A123 XYZ");
-        //ccs.vehicleEnteringZone(Vehicle vehicle);
-
-    }
-
-    @Test
-    public void checkVehicleLeavingZone(){
-
-    }
-
->>>>>>> 39806bbab715494ead85c0b9beab053fb086a69e
 //    public void checkMinutesBetween(){
 //        CongestionChargeSystem ccs = new CongestionChargeSystem();
 //        assertEquals(ccs.minutesBetween(), 1L);
