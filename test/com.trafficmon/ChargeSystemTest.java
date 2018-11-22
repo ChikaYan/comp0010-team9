9d4c88375087e267d30b49edcc5b1efc00c27ef9
Walter Wu <walterwuyan@gmail.com>
2018-11-22 12:21:34 +0000

    Wrote tests for original charge system

--- test/com.trafficmon/ChargeSystemTest.java
+++ test/com.trafficmon/ChargeSystemTest.java
@@ -1,20 +1,42 @@
 package com.trafficmon;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class ChargeSystemTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
+
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final CongestionChargeSystem chargeSystem = new CongestionChargeSystem();
+
+    @Before
+    public void setUpSystemOut(){
+        System.setOut(new PrintStream(output));
+    }
 
     @Test
     public void enterBefore2AndStayUpTo4IsChargedFor6(){
         
     }
 
+
+    @Test
+    public void oldSystemCharges5pEveryMinRoundUp() throws InterruptedException {
+        chargeSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"));
+        Thread.sleep( 1000);
+        chargeSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"));
+        chargeSystem.calculateCharges();
+
+        assertTrue(output.toString().contains("Charge made to account of Fred Bloggs, £0.05 deducted, balance:"));
+    }
 
 //    @Test
 //    public void checkVehicleEnteringZone(){
