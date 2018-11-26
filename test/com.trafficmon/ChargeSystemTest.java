3dfd3902b501c53b39cad6256cfa62df3691fd4f
Supernano233 <32653444+Supernano233@users.noreply.github.com>
2018-11-26 18:30:08 +0000

    Add test for charges after2 in different cases

--- test/com.trafficmon/ChargeSystemTest.java
+++ test/com.trafficmon/ChargeSystemTest.java
@@ -32,7 +32,7 @@ public class ChargeSystemTest {
 
     @Test
     public void enterBefore2AndStayUpTo4IsChargedFor6() throws AccountNotRegisteredException, InsufficientCreditException {
-        MockClock mockClock = new MockClock(6, 0);
+        MockClock mockClock = new MockClock(6, 0); //enter at 6:00
         CongestionChargeSystem chargeSystem =
                 new CongestionChargeSystem(mockPayment, mockClock);
         chargeSystem.vehicleEnteringZone(testVehicle);
@@ -40,6 +40,70 @@ public class ChargeSystemTest {
         chargeSystem.vehicleLeavingZone(testVehicle);
         context.checking(new Expectations() {{
             oneOf(mockPayment).deductCharge(testVehicle, 6);
+        }});
+        chargeSystem.calculateCharges();
+    }
+
+    @Test
+    public void enterAndGoOutBefore2ThenGoBackAfter2AndStayUpTo4IsChargedFor6()throws AccountNotRegisteredException, InsufficientCreditException{
+        MockClock mockClock = new MockClock(12, 0);     //enter at 12:00
+        CongestionChargeSystem chargeSystem =
+                new CongestionChargeSystem(mockPayment, mockClock);
+        chargeSystem.vehicleEnteringZone(testVehicle); //12:00 in
+        mockClock.advanceBy(1, 0);
+        chargeSystem.vehicleLeavingZone(testVehicle); //13:00 out
+        mockClock.advanceBy(1, 0);
+        chargeSystem.vehicleEnteringZone(testVehicle);  //14:00 in
+        mockClock.advanceBy(1, 0);
+        chargeSystem.vehicleLeavingZone(testVehicle); //15:00 out
+        context.checking(new Expectations() {{
+            oneOf(mockPayment).deductCharge(testVehicle, 6);
+        }});
+        chargeSystem.calculateCharges();
+    }
+
+    @Test
+    public void enterAfter2AndStayUpTo4IsChargedFor4()throws AccountNotRegisteredException, InsufficientCreditException{
+        MockClock mockClock = new MockClock(15, 0); //enter at 15:00
+        CongestionChargeSystem chargeSystem =
+                new CongestionChargeSystem(mockPayment, mockClock);
+        chargeSystem.vehicleEnteringZone(testVehicle);
+        mockClock.advanceBy(2, 0);
+        chargeSystem.vehicleLeavingZone(testVehicle);
+        context.checking(new Expectations() {{
+            oneOf(mockPayment).deductCharge(testVehicle, 4);
+        }});
+        chargeSystem.calculateCharges();
+    }
+
+    @Test
+    public void enterAfter2AndGoOutThenGoBackUpTo4IsChargedFor4()throws AccountNotRegisteredException, InsufficientCreditException{
+        MockClock mockClock = new MockClock(15, 0);
+        CongestionChargeSystem chargeSystem =
+                new CongestionChargeSystem(mockPayment, mockClock);
+        chargeSystem.vehicleEnteringZone(testVehicle);//15:00 In
+        mockClock.advanceBy(1, 0);
+        chargeSystem.vehicleLeavingZone(testVehicle);//16:00 Out
+        mockClock.advanceBy(1, 0);
+        chargeSystem.vehicleEnteringZone(testVehicle);//17:00 In
+        mockClock.advanceBy(1, 0);
+        chargeSystem.vehicleLeavingZone(testVehicle);//18:00 Out
+        context.checking(new Expectations() {{
+            oneOf(mockPayment).deductCharge(testVehicle, 4);
+        }});
+        chargeSystem.calculateCharges();
+    }
+
+    @Test
+    public void enterAfter2AndStayUpTo5IsChargedFor12()throws AccountNotRegisteredException, InsufficientCreditException{
+        MockClock mockClock = new MockClock(15, 0);
+        CongestionChargeSystem chargeSystem =
+                new CongestionChargeSystem(mockPayment, mockClock);
+        chargeSystem.vehicleEnteringZone(testVehicle);
+        mockClock.advanceBy(5, 0);
+        chargeSystem.vehicleLeavingZone(testVehicle);
+        context.checking(new Expectations() {{
+            oneOf(mockPayment).deductCharge(testVehicle, 12);
         }});
         chargeSystem.calculateCharges();
     }
