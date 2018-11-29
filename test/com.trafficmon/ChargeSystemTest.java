82a72da13f14d5f07b56fd954dab23111e380467
Walter Wu <walterwuyan@gmail.com>
2018-11-29 08:57:50 +0000

    Separated old tests to different file

--- test/com.trafficmon/ChargeSystemTest.java
+++ test/com.trafficmon/ChargeSystemTest.java
@@ -19,16 +19,8 @@ public class ChargeSystemTest {
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
     private final AccountManager mockPayment = context.mock(AccountManager.class);
-    private final Clock clock = context.mock(Clock.class);
 
-    private final CongestionChargeSystem chargeSystem = new CongestionChargeSystem();
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     private final Vehicle testVehicle = Vehicle.withRegistration("A123 XYZ");
-
-    @Before
-    public void setUpSystemOut() {
-        System.setOut(new PrintStream(output));
-    }
 
     @Test
     public void enterBefore2AndStayUpTo4IsChargedFor6() throws AccountNotRegisteredException, InsufficientCreditException {
@@ -45,7 +37,7 @@ public class ChargeSystemTest {
     }
 
     @Test
-    public void enterAndGoOutBefore2ThenGoBackAfter2AndStayUpTo4IsChargedFor6()throws AccountNotRegisteredException, InsufficientCreditException{
+    public void enterAndGoOutBefore2ThenGoBackAfter2AndStayUpTo4IsChargedFor6() throws AccountNotRegisteredException, InsufficientCreditException {
         MockClock mockClock = new MockClock(12, 0);     //enter at 12:00
         CongestionChargeSystem chargeSystem =
                 new CongestionChargeSystem(mockPayment, mockClock);
@@ -63,7 +55,7 @@ public class ChargeSystemTest {
     }
 
     @Test
-    public void enterAfter2AndStayUpTo4IsChargedFor4()throws AccountNotRegisteredException, InsufficientCreditException{
+    public void enterAfter2AndStayUpTo4IsChargedFor4() throws AccountNotRegisteredException, InsufficientCreditException {
         MockClock mockClock = new MockClock(15, 0); //enter at 15:00
         CongestionChargeSystem chargeSystem =
                 new CongestionChargeSystem(mockPayment, mockClock);
@@ -77,7 +69,7 @@ public class ChargeSystemTest {
     }
 
     @Test
-    public void enterAfter2AndGoOutThenGoBackAndStayUpTo4IsChargedFor4()throws AccountNotRegisteredException, InsufficientCreditException{
+    public void enterAfter2AndGoOutThenGoBackAndStayUpTo4IsChargedFor4() throws AccountNotRegisteredException, InsufficientCreditException {
         MockClock mockClock = new MockClock(15, 0);
         CongestionChargeSystem chargeSystem =
                 new CongestionChargeSystem(mockPayment, mockClock);
@@ -95,7 +87,7 @@ public class ChargeSystemTest {
     }
 
     @Test
-    public void enterAfter2AndStayUpTo5IsChargedFor12()throws AccountNotRegisteredException, InsufficientCreditException{
+    public void enterAfter2AndStayUpTo5IsChargedFor12() throws AccountNotRegisteredException, InsufficientCreditException {
         MockClock mockClock = new MockClock(15, 0);
         CongestionChargeSystem chargeSystem =
                 new CongestionChargeSystem(mockPayment, mockClock);
@@ -110,7 +102,6 @@ public class ChargeSystemTest {
 
 
     private class MockClock implements Clock {
-
         private LocalTime time;
 
         MockClock(int hour, int min) {
@@ -122,67 +113,12 @@ public class ChargeSystemTest {
             return time;
         }
 
-        public void advanceBy(int hour, int min) {
+        void advanceBy(int hour, int min) {
             time = time.plusHours(hour).plusMinutes(min);
         }
     }
 
     //--------------------------OLD TESTS---------------------------
 
-//    @Test
-//    public void oldSystemCharges5pEveryMinRoundUp() throws InterruptedException {
-//        chargeSystem.vehicleEnteringZone(testVehicle);
-//        Thread.sleep(1000);
-//        chargeSystem.vehicleLeavingZone(testVehicle);
-//        chargeSystem.calculateCharges();
-//        assertTrue(output.toString().contains(
-//                "Charge made to account of Fred Bloggs, £0.05 deducted, balance:"));
-//    }
 
-    @Test
-    public void mismatchedEntryExitsTriggerInvestigation() throws InterruptedException {
-//        chargeSystem.vehicleEnteringZone(testVehicle); // not necessary as all vehicles leave before charge is deducted
-//        chargeSystem.calculateCharges();
-//        assertTrue(output.toString().contains(
-//                "Charge made to account of Fred Bloggs, £0.00 deducted, balance:"));
-//        output.reset();
-        chargeSystem.vehicleEnteringZone(testVehicle);
-        chargeSystem.vehicleEnteringZone(testVehicle);
-        chargeSystem.calculateCharges();
-        assertEquals(output.toString(),
-                "Mismatched entries/exits. Triggering investigation into vehicle: Vehicle [A123 XYZ]\r\n");
-        output.reset();
-        chargeSystem.vehicleLeavingZone(testVehicle);
-        chargeSystem.calculateCharges();
-        assertEquals(output.toString(),
-                "Mismatched entries/exits. Triggering investigation into vehicle: Vehicle [A123 XYZ]\r\n");
-    }
-
-    @Test
-    public void insufficientCreditTriggersPenalty() throws InterruptedException {
-        chargeSystem.vehicleEnteringZone(Vehicle.withRegistration("none-exist vehicle"));
-        Thread.sleep(1000);
-        chargeSystem.vehicleLeavingZone(Vehicle.withRegistration("none-exist vehicle"));
-        chargeSystem.calculateCharges();
-        assertEquals(output.toString(),
-                "Penalty notice for: Vehicle [none-exist vehicle]\r\n");
-
-    }
-
-//    @Test
-//    public void checkVehicleEnteringZone(){
-//        CongestionChargeSystem ccs = new CongestionChargeSystem();
-//        Vehicle vehicle = Vehicle.withRegistration("A123 XYZ");
-//        //ccs.vehicleEnteringZone(Vehicle vehicle);
-//
-//    }
-//
-//    @Test
-//    public void checkVehicleLeavingZone(){
-//
-//    }
-//    public void checkMinutesBetween(){
-//        CongestionChargeSystem ccs = new CongestionChargeSystem();
-//        assertEquals(ccs.minutesBetween(), 1L);
-//    }
 }
---
+++ test/com.trafficmon/OldSystemIntegratedTest.java
@@ -0,0 +1,78 @@
+package com.trafficmon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class OldSystemIntegratedTest {
+    private final CongestionChargeSystem chargeSystem = new CongestionChargeSystem();
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final Vehicle testVehicle = Vehicle.withRegistration("A123 XYZ");
+
+
+    @Before
+    public void setUpSystemOut() {
+        System.setOut(new PrintStream(output));
+    }
+
+    //    @Test
+//    public void oldSystemCharges5pEveryMinRoundUp() throws InterruptedException {
+//        chargeSystem.vehicleEnteringZone(testVehicle);
+//        Thread.sleep(1000);
+//        chargeSystem.vehicleLeavingZone(testVehicle);
+//        chargeSystem.calculateCharges();
+//        assertTrue(output.toString().contains(
+//                "Charge made to account of Fred Bloggs, £0.05 deducted, balance:"));
+//    }
+
+    @Test
+    public void mismatchedEntryExitsTriggerInvestigation(){
+//        chargeSystem.vehicleEnteringZone(testVehicle); // not necessary as all vehicles leave before charge is deducted
+//        chargeSystem.calculateCharges();
+//        assertTrue(output.toString().contains(
+//                "Charge made to account of Fred Bloggs, £0.00 deducted, balance:"));
+//        output.reset();
+        chargeSystem.vehicleEnteringZone(testVehicle);
+        chargeSystem.vehicleEnteringZone(testVehicle);
+        chargeSystem.calculateCharges();
+        assertEquals(output.toString(),
+                "Mismatched entries/exits. Triggering investigation into vehicle: Vehicle [A123 XYZ]\r\n");
+        output.reset();
+        chargeSystem.vehicleLeavingZone(testVehicle);
+        chargeSystem.calculateCharges();
+        assertEquals(output.toString(),
+                "Mismatched entries/exits. Triggering investigation into vehicle: Vehicle [A123 XYZ]\r\n");
+    }
+
+    @Test
+    public void insufficientCreditTriggersPenalty() throws InterruptedException {
+        chargeSystem.vehicleEnteringZone(Vehicle.withRegistration("none-exist vehicle"));
+        Thread.sleep(1000);
+        chargeSystem.vehicleLeavingZone(Vehicle.withRegistration("none-exist vehicle"));
+        chargeSystem.calculateCharges();
+        assertEquals(output.toString(),
+                "Penalty notice for: Vehicle [none-exist vehicle]\r\n");
+
+    }
+
+//    @Test
+//    public void checkVehicleEnteringZone(){
+//        CongestionChargeSystem ccs = new CongestionChargeSystem();
+//        Vehicle vehicle = Vehicle.withRegistration("A123 XYZ");
+//        //ccs.vehicleEnteringZone(Vehicle vehicle);
+//
+//    }
+//
+//    @Test
+//    public void checkVehicleLeavingZone(){
+//
+//    }
+//    public void checkMinutesBetween(){
+//        CongestionChargeSystem ccs = new CongestionChargeSystem();
+//        assertEquals(ccs.minutesBetween(), 1L);
+//    }
+}
