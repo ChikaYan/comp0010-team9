c2d3aa9f5fd7809a9ace7a0348898cbbc18ee2ac
Walter Wu <walterwuyan@gmail.com>
2018-12-03 14:26:02 +0000

    Modified CHARGE

--- test/com.trafficmon/PaymentSystemTest.java
+++ test/com.trafficmon/PaymentSystemTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PaymentSystemTest {
-    private final int CHARGE = 3;
+    private final int CHARGE = 1;
     private final AccountManager payment = new PaymentSystem();
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     private final Vehicle testVehicle = Vehicle.withRegistration("A123 XYZ");
@@ -29,28 +29,28 @@ public class PaymentSystemTest {
 
     @Test
     public void canDeductCharge() throws AccountNotRegisteredException, InsufficientCreditException {
-        payment.deductCharge(testVehicle,CHARGE);
+        payment.deductCharge(testVehicle, CHARGE);
         assertTrue(output.toString().contains(
-                "Charge made to account of Fred Bloggs, £3.00 deducted, balance:"));
+                "Charge made to account of Fred Bloggs, £1.00 deducted, balance:"));
     }
 
     @Test(expected = AccountNotRegisteredException.class)
     public void canThrowAccountNotRegisteredException() throws AccountNotRegisteredException, InsufficientCreditException {
-        payment.deductCharge(Vehicle.withRegistration("random vehicle"),CHARGE);
+        payment.deductCharge(Vehicle.withRegistration("random vehicle"), CHARGE);
         assertEquals(output.toString(),
                 "Penalty notice for: Vehicle [random vehicle]\r\n");
     }
 
     @Test(expected = InsufficientCreditException.class)
     public void canThrowInsufficientCreditException() throws AccountNotRegisteredException, InsufficientCreditException {
-        payment.deductCharge(testVehicle,1000);
+        payment.deductCharge(testVehicle, 1000);
         assertEquals(output.toString(),
                 "Penalty notice for: Vehicle [random vehicle]\r\n");
     }
 
     @Test
-    public void canIssuePenalty(){
-        payment.issuePenalty(testVehicle,CHARGE);
+    public void canIssuePenalty() {
+        payment.issuePenalty(testVehicle, CHARGE);
         assertEquals(output.toString(),
                 "Penalty notice for: Vehicle [A123 XYZ]\r\n");
     }
