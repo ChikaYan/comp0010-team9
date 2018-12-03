afa23718798189bf7a64c45965836b20c5b9e4d1
Walter Wu <walterwuyan@gmail.com>
2018-12-03 14:25:51 +0000

    Extracted fields

--- src/com/trafficmon/PaymentSystem.java
+++ src/com/trafficmon/PaymentSystem.java
@@ -3,19 +3,22 @@ package com.trafficmon;
 import java.math.BigDecimal;
 
 public class PaymentSystem implements AccountManager {
+
+    private PenaltiesService operationTeam = OperationsTeam.getInstance();
+    private AccountsService accountsService = RegisteredCustomerAccountsService.getInstance();
+
     @Override
     public void triggerInvestigationInto(Vehicle vehicle) {
-        OperationsTeam.getInstance().triggerInvestigationInto(vehicle);
+        operationTeam.triggerInvestigationInto(vehicle);
     }
 
     @Override
     public void deductCharge(Vehicle vehicle, int charge) throws AccountNotRegisteredException, InsufficientCreditException {
-        RegisteredCustomerAccountsService.getInstance()
-                .accountFor(vehicle).deduct(BigDecimal.valueOf(charge));
+        accountsService.accountFor(vehicle).deduct(BigDecimal.valueOf(charge));
     }
 
     @Override
     public void issuePenalty(Vehicle vehicle, int charge) {
-        OperationsTeam.getInstance().issuePenaltyNotice(vehicle, BigDecimal.valueOf(charge));
+        operationTeam.issuePenaltyNotice(vehicle, BigDecimal.valueOf(charge));
     }
 }
